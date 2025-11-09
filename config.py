9671e960cf093fdeb1acf5b227342e627e48e258
taylantalu1 <taylantalu@hotmail.com>
2025-11-09 10:00:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-import os
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+# config.py.example
+# Kendi tokenınızı buraya koymayın. GitHub'a yüklemeyin.
+# Deploy ederken Render veya local environment değişkeni ile BOT_TOKEN ayarlayın.
 
+BOT_TOKEN = "8549214864:AAGEaInkNFRpI126JXxQvBlphzI3WTIfJ-U"
