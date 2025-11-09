cdb303bdca86d758cbd21d5bb19e57364cb5b925
taylantalu1 <taylantalu@hotmail.com>
2025-11-09 12:31:53 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py.example
-# Kendi tokenınızı buraya koymayın. GitHub'a yüklemeyin.
-# Deploy ederken Render veya local environment değişkeni ile BOT_TOKEN ayarlayın.
-
-BOT_TOKEN = "8549214864:AAGEaInkNFRpI126JXxQvBlphzI3WTIfJ-U"
